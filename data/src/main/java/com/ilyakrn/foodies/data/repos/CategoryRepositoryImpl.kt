a68216d892604e543140cc5314fc456c25a4c1d7
IlyaKrn <ilyakornoukhov@gmail.com>
2024-03-31 00:05:11 +0300

    data + usecases

--- data/src/main/java/com/ilyakrn/foodies/data/repos/CategoryRepositoryImpl.kt
+++ data/src/main/java/com/ilyakrn/foodies/data/repos/CategoryRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.ilyakrn.foodies.data.repos
 
 import android.util.Log
 import com.ilyakrn.foodies.data.retrofit.RetrofitClient
+import com.ilyakrn.foodies.data.retrofit.models.CategoryJsonModel
 import com.ilyakrn.foodies.data.retrofit.services.RetorfitCategoryService
 import com.ilyakrn.foodies.data.retrofit.services.RetorfitProductService
 import com.ilyakrn.foodies.domain.models.core.Category
@@ -15,16 +16,20 @@ class CategoryRepositoryImpl : CategoryRepository {
     override fun getCategoryList(listener: (List<Category>) -> Unit) {
         val call = RetrofitClient.getClient().create(RetorfitCategoryService::class.java).getCategoryList()
 
-        call.enqueue(object : Callback<List<Category>> {
-            override fun onResponse(call: Call<List<Category>>, response: Response<List<Category>>) {
+        call.enqueue(object : Callback<List<CategoryJsonModel>> {
+            override fun onResponse(call: Call<List<CategoryJsonModel>>, response: Response<List<CategoryJsonModel>>) {
                 if(response.isSuccessful && response.body() != null){
-                    listener(response.body()!!)
+                    val res = ArrayList<Category>()
+                    response.body()!!.forEach {
+                        res.add(Category(it.id, it.name))
+                    }
+                    listener(res)
                 }else{
                     listener(ArrayList())
                 }
             }
 
-            override fun onFailure(call: Call<List<Category>>, t: Throwable) {
+            override fun onFailure(call: Call<List<CategoryJsonModel>>, t: Throwable) {
                 listener(ArrayList())
             }
         })
